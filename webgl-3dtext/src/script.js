adb88601791398277be33c6c516b338fe1bc78a8
hazelnutcloud <ha5anmukhli50@gmail.com>
2022-01-30 00:11:47 +0800

    adjusted initial camera position

--- webgl-3dtext/src/script.js
+++ webgl-3dtext/src/script.js
@@ -105,9 +105,9 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 3
+camera.position.x = 0
+camera.position.y = 0.5
+camera.position.z = 5
 scene.add(camera)
 
 // Controls
